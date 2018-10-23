673af4cc094573ef8a663f62e4da8d10c9d145bc
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-23 19:11:41 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
   #code your solution here:
 
-  
+  pic_tragedy[:montague][:patriarch][:name] = 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
